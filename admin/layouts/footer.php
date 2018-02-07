d1b6642e27aa9d424a810bcf2ff44af5f4abbe1b
Charuna Madubasha Kotawalagedara <charunak1@gmail.com>
2018-02-08 00:58:27 +0530

    Add files via upload

---
+++ admin/layouts/footer.php
@@ -0,0 +1,4 @@
+<footer>
+  eZyQuiz online examination system is a free to use, open source.<br>
+  eZyQuiz online examination system created by CHARUNA MADUBASHA KOTWALAGEDARA
+</footer>
---
+++ admin/layouts/top_nav.php
@@ -0,0 +1,16 @@
+<nav class="navbar navbar-inverse navbar-fixed-top">
+<div class="container-fluid">
+  <div class="navbar-header">
+    <a class="navbar-brand" href="Instruction.php">Admin Panel</a>
+  </div>
+  <ul class="nav navbar-nav">
+    <li><a href="exam_customize.php">EXAM</a></li>
+    <li><a href="question_view.php">QUESTION</a></li>
+    <li><a href="candidate_view.php">CANDIDATE</a></li>
+  </ul>
+  <ul class="nav navbar-nav navbar-right">
+    <li><a href="update_password.php"><span class="glyphicon glyphicon-edit"></span> UPDATE PASSWORD</a></li>
+    <li><a href="logout.php"><span class="glyphicon glyphicon-log-out"></span> LOGOUT</a></li>
+  </ul>
+</div>
+</nav>
